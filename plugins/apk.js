72f6fc66e2a544e660e3279cc77e9c4462b0f61f
sayura19 <gojom451@gmail.com>
2025-06-04 18:22:28 +0530

    Create apk.js

---
+++ plugins/apk.js
@@ -0,0 +1,50 @@
+const { cmd, commands } = require('../lib/command');
+const { ytsearch, ytmp3, ytmp4 } = require('@dark-yasiya/yt-dl.js');
+const scraper = require("../lib/scraper");
+const axios = require('axios');
+const fetch = require('node-fetch');
+const { fetchJson, getBuffer } = require('../lib/functions');
+const { lookup } = require('mime-types');
+const fs = require('fs');
+const path = require('path');
+
+
+cmd({
+    pattern: "apk",
+    desc: "Downloads Apk",
+    use: ".apk <app_name>",
+    react: "📥",
+    category: "download",
+    filename: __filename
+},
+    async (conn, mek, m, { from, q, reply }) => {
+        const appId = q.trim();
+    if (!appId) return reply(`*Please provide an app name 📍*`);
+
+    reply("*Downloading Your Apk...📥*");
+          
+    try {
+        const appInfo = await scraper.aptoideDl(appId);
+        const buff = await getBuffer(appInfo.link);
+        
+        if (!buff || !appInfo.appname) {
+            return await conn.sendMessage(from, { react: { text: '❌', key: mek.key } });
+        }
+        
+        await conn.sendMessage(
+            from,
+            { document: buff, 
+             caption: `> *© Upload By ACD MD V2*`, 
+             mimetype: "application/vnd.android.package-archive", 
+             filename: `${appInfo.appname}.apk`,
+           }, { quoted: mek }
+        );
+        
+        await conn.sendMessage(from, { react: { text: '✅', key: mek.key } });
+        reply("*Upload Successful ✅*");
+    } catch (e) {
+        console.log(e);
+        await conn.sendMessage(from, { react: { text: '❌', key: mek.key } });
+        reply(`Error: ${e.message}`);
+    }
+});
